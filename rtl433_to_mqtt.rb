40e8e85da5db08870e79e545034a22dd015816aa
Dayne Broderson <broderson@gmail.com>
2019-08-05 12:05:36 -0800

    adding log option and skip dups

--- rtl433_to_mqtt.rb
+++ rtl433_to_mqtt.rb
@@ -5,6 +5,16 @@ require 'json'
 require 'pty'
 require 'colored'
 require 'yaml'
+require 'logger'
+
+if ARGV.delete("-l")
+  logger = Logger.new('rtl433.log','daily')
+  logger.formatter = proc do |sev, datetime, progname, msg|
+    # sev and progname not helpful. datetime already in msg so just
+    # slam the json messages in log file - one msg line
+    "#{msg}\n" 
+  end
+end
 
 cfg = YAML.load_file('config.yml')
 
@@ -16,12 +26,17 @@ mqtt = MQTT::Client.connect(
 begin
   PTY.spawn("rtl_433 -G -F json") do |stdout, stdin, pid|
     begin
+      last_line = ""
       stdout.each do |line|
         begin
+          # some sensors send the same message multiple times - skip dups
+          next if line == last_line
           v = JSON.parse(line)
           v['ts'] = Time.now.getutc.to_i
           puts v.inspect.green
           mqtt.publish(cfg['topic'], v.to_json)
+          logger.info(v.to_json) if logger
+          last_line = line
         rescue JSON::ParserError
           puts "Unparsable: #{line}".yellow
         end
