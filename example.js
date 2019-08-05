ce664b3b44bd1ff9898161ed1ca52c77ecdce855
Dayne Broderson <broderson@gmail.com>
2019-08-05 12:08:28 -0800

    adding an example of the mqtt output

---
+++ example.js
@@ -0,0 +1,3 @@
+{"time"=>"2019-07-18 12:31:09", "model"=>"Acurite tower sensor", "id"=>5748, "sensor_id"=>5748, "channel"=>"A", "temperature_C"=>23.2, "humidity"=>53, "battery_low"=>0, "ts"=>1563481870}
+{"time"=>"2019-07-18 12:31:16", "model"=>"Acurite 606TX Sensor", "id"=>89, "battery"=>"OK", "temperature_C"=>23.3, "mic"=>"CHECKSUM", "ts"=>1563481877}      
+{"time"=>"2019-07-18 12:31:47", "model"=>"Oregon Scientific SL109H", "id"=>0, "channel"=>3, "temperature_C"=>0.0, "humidity"=>0, "status"=>0, "mic"=>"CHECKSUM", "ts"=>1563481908}
