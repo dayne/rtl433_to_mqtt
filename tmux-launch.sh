4027efd9c3836e6c9aba4ba2f8bbad89eb57a9fd
Dayne Broderson <broderson@gmail.com>
2019-08-05 12:07:57 -0800

    report how to attach instead of attaching

--- tmux-launch.sh
+++ tmux-launch.sh
@@ -5,9 +5,10 @@
 tmux list-sessions | grep rtl2mqtt > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	echo "session already started"
+	echo "use: tmux attach -t rtl2mqtt:launcher"
 	exit 1
 fi
 
 tmux new-session -d -s rtl2mqtt -n launcher
 tmux send-keys -t rtl2mqtt:launcher "$(dirname ${0})/launch.sh" Enter
-tmux attach -t rtl2mqtt:launcher
+echo "attach to tmux with:  tmux attach -t rtl2mqtt:launcher"
