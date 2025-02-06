09bbc0ee9a77ec26f99590aa6fe9e48907ab0e3d
Hailey Hodgins <haileyhodgins@gmail.com>
2025-02-05 17:08:16 -0900

    hhack launch.sh

--- launch.sh
+++ launch.sh
@@ -16,7 +16,12 @@ fi
 while(true); do
   log "starting rtl433_to_mqtt"
   ./rtl433_to_mqtt.rb -l
-  log "rtl433_to_mqtt crashed / killed"
-  echo "relaunching in 5 seconds - double check USB receiver is fully plugged in"
-  sleep 5;
+  if [ $? -eq 0 ]; then
+	log "rtl433_to_mqtt existed gracefully"
+	exit 1
+  else
+        log "rtl433_to_mqtt crashed / killed"
+  	echo "relaunching in 5 seconds - double check USB receiver is fully plugged in"
+  	sleep 5;
+  fi
 done
